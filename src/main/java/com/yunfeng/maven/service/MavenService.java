590f2678493f96fee5451e32fb2ec630fe622311
xiayunfeng <xiayunfeng2012@gmail.com>
2018-11-26 11:21:27 +0800

    add service

--- src/main/java/com/yunfeng/maven/service/MavenService.java
+++ src/main/java/com/yunfeng/maven/service/MavenService.java
@@ -16,10 +16,13 @@ public class MavenService {
     public Collection<MavenProxyUrl> getProxyUrls() {
         Collection<MavenProxyUrl> c = mavenRepository.findAll();
         if (c.isEmpty()) {
-            c.add(new MavenProxyUrl("google", "https://dl.google.com/dl/android/maven2/"));
-            c.add(new MavenProxyUrl("jcenter", "https://jcenter.bintray.com/"));
             c.add(new MavenProxyUrl("mavenRepo1", "https://repo1.maven.org/maven2/"));
+            c.add(new MavenProxyUrl("mavenRepo", "http://repo1.maven.org/maven2/"));
+            c.add(new MavenProxyUrl("sonatypeRepo", "http://oss.sonatype.org/content/repositories/releases/"));
+            c.add(new MavenProxyUrl("apacheSnapshots", "https://repository.apache.org/content/repositories/snapshots/"));
             c.add(new MavenProxyUrl("mavenCenter", "http://central.maven.org/maven2/"));
+            c.add(new MavenProxyUrl("jcenter", "https://jcenter.bintray.com/"));
+            c.add(new MavenProxyUrl("google", "https://dl.google.com/dl/android/maven2/"));
             mavenRepository.saveAll(c);
         }
         return c;
